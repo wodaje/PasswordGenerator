3644de81fa0f4a6a357df8854a34611fa87efa7b
Jeff Woda <wodahouse@gmail.com>
2020-11-13 21:40:45 -0500

    added switch logic for password string selection

--- readme.md
+++ readme.md
@@ -1,1 +1,9 @@
 Password Generator Upgrades/Functionality added Jeff Woda Nov 14 2020 Assignment
+
+Program first confirms user selection to proceed with password generation - if selected in error nothing is enacted and porgram is terminated with message
+
+User then chooses password length = choice is confirmed
+
+User then selects password criteria - note a choice must be made or the choices will continue to loop until at least one choice is selected - else error message will be displayed - there is no way to exit from this choice
+
+Choice composite of User selected Password is displayed
--- script.js
+++ script.js
@@ -9,9 +9,9 @@ var numChar = ['0','1','2','3','4','5','6','7','8','9']
 
 // Write password to the #password input
   function writePassword() {
-  var initialChoice = confirm("Are you looking to generate a random secure password? ")  
 
-  // User Choice to continue with password generator
+// User Choice to continue with password generator
+  var initialChoice = confirm("Are you looking to generate a random secure password? ")  
   if (initialChoice) {
 
   var lengthPass = prompt("Enter desired length in numbers between 8 and 128")
@@ -22,28 +22,95 @@ var numChar = ['0','1','2','3','4','5','6','7','8','9']
   //This will confirm user choices and circle back if non are selected
   
   for ( var x = 0; x < 1; x=x)  {
+   
+    //y is to identify user combo for password selection
+    var y = 0
 
   var lowerCharChoice   = confirm("Would you like to enclose Lower Case Characters ?")
   if (lowerCharChoice){
       x=x+1
+      y = y + .0001
    }
-  var upperCharChoice   = confirm("Would you like to include Upper Case Characters ?")
+ 
+   var upperCharChoice   = confirm("Would you like to include Upper Case Characters ?")
    if (upperCharChoice){
      x=x+1
+     y = y + .001 
    }
-  var specialCharChoice = confirm("Would you like Special Characters Included ?")
+ 
+   var specialCharChoice = confirm("Would you like Special Characters Included ?")
    if (specialCharChoice){
       x=x+1
+      y = y + .01
    }
-  var numCharChoice     = confirm("Would you like numbers included ?")
+  
+   var numCharChoice     = confirm("Would you like numbers included ?")
     if (numCharChoice){
       x=x+1
+      y = y + .1
    } 
    if (x < 1)  {
      alert("Error = You must select at least one Character set to continue! Please try again")
    }
   }
 
+  // match string combo for random password generation
+  var stringSelector = ""
+
+switch(y){
+    case .0001:
+        var stringSelector = lowerChar;
+        break;
+    case .0010:
+        var stringSelector =  upperChar;
+        break;
+    case .0011:
+        var stringSelector =  lowerChar.concat(upperChar);
+        break;
+    case .0100:
+        var stringSelector =  specialChar;
+        break;
+    case .0101:
+        var stringSelector =  lowerChar.concat(specialChar);
+        break;
+    case .0110:
+        var stringSelector =  upperChar.concat(specialChar);
+        break;
+    case .0111:
+        var stringSelector =  lowerChar.concat(upperChar,specialChar);
+        break;
+    case .1000:
+        var stringSelector =  numChar;
+        break;
+    case .1001:
+        var stringSelector =  lowerChar.concat(numChar);
+        break;
+    case .1010:
+        var stringSelector =  upperChar.concat(numChar);
+        break;    
+    case .1011:
+        var stringSelector =  lowerChar.concat(upperChar,numChar); 
+        break; 
+    case .1100:
+        var stringSelector =  specialChar.concat(numChar); 
+        break;    
+    case .1101:
+        var stringSelector =  lowerChar.concat(specialChar,numChar);
+        break; 
+    case .1110:
+        var stringSelector =  upperChar.concat(specialChar,numChar);
+        break;
+    case .1111:
+        var stringSelector =  lowerChar.concat(upperChar,specialChar,numChar);
+    }  
+
+
+alert(stringSelector)
+
+ //Selecting combined array to generate password 
+//array1.concat(array2, array3, ..., arrayX)
+// syntax for picking a random item from an array
+//var randomItem = arr[Math.floor(Math.random() * arr.length)]
 
 
 
